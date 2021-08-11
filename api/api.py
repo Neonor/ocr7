ddb269e65c46f55f800af0153ec77dabf46eae53
neonor <neonor@neonor.org>
2021-08-11 21:22:36 +0200

    V2.11

--- api/api.py
+++ api/api.py
@@ -1,20 +1,13 @@
+from os.path import join,dirname
+from pickle import load
 import pandas as pd
-import numpy as np
 import gc
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import KFold
+# from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder, MultiLabelBinarizer
+# from sklearn.compose import make_column_transformer
 
-
-from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder, MultiLabelBinarizer
-from sklearn.compose import make_column_transformer
-
-import lightgbm as lgb
-
-from pickle import load,loads
-
-from os.path import join,dirname
+# import lightgbm as lgb
 
 
 from fastapi import FastAPI
@@ -25,17 +18,25 @@ def home():
     return {"projet":'OCR7'}
 
 @app.get('/compute/')
-def compute():
-    for data in pd.read_csv("srv/api/computed.xz",compression="xz",chunksize=100000):
-        user = data[data.SK_ID_CURR == idc].copy()
+def compute(ID:int):
+#     idc = int(request.args.get('ID'))
+    for data in pd.read_csv(join(dirname(__file__),"computed.xz"),index_col=0,compression="xz",chunksize=100000):
+        user = data[data.SK_ID_CURR == ID].copy()
+        user.pop("TARGET")
+        del data
+        gc.collect()
         if len(user):
             with open(join(dirname(__file__),"std.pk"),"rb") as f_std:
                 standardize = load(f_std)
+                std = standardize.transform(user)
+                del standardize
+                gc.collect()
 
             with open(join(dirname(__file__),"model.pk"),"rb") as f_model:
                 model = load(f_model)
-            user.pop("TARGET")
-            idc = int(request.args.get('ID'))
-            return list(model.predict_proba(standardize.transform(user))[0])
-        del data
-        gc.collect()
+                extract = model.predict_proba(std)
+                del model
+                gc.collect()
+                
+            return list(extract[0])
+
